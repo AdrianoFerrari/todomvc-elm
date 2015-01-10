eae4b9256dee7ea47cf57314ae74b02731bcaa44
Adriano Ferrari <adriano.ferrari@gmail.com>
2015-01-10 07:48:11 -0500

    Some Elm written. Haven't compiled yet.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-README.md
+# My TodoMVC with Elm
+
+Rules: can copy file structure and non-Elm files from `elm-todomvc`, but all Elm code must be from memory, or using library reference (not using Todo.elm as ref). Can use other TodoMVC libs as reference too.
--- Todo.elm
+++ Todo.elm
@@ -1,1 +1,49 @@
-{- everything goes here -}
+module Todo where
+
+import Html
+import List
+import Signal
+import String
+
+{---- State ----}
+type Visibility = All | Active | Completed
+
+type alias State =
+  { todos: List Todo
+  , visibility: Visibility
+  , uid: Int
+  }
+
+type alias Todo =
+  { title: String
+  , completed: Bool
+  , id: Int
+  }
+
+{---- Evolution ----}
+type Delta  = NoOp
+            | TodoAdd Todo
+            | TodoDelete Int
+            | TodoToggle Int
+            | SetFilter Visibility
+
+update : State -> Delta -> State
+update state delta =
+  case delta of
+    NoOp -> state
+
+    TodoAdd todo ->
+      { state | state.todos <- state.todos ++ todo }
+
+    TodoDelete id ->
+      { state | state.todos <- List.filter (\t -> not (t.id == id)) state.todos }
+
+    TodoToggle id ->
+      { state | List.map (\t -> if t.id then t.completed <- not t.complete) state.todos}
+
+    SetFilter vis ->
+      { state | visibility <- vis }
+
+{---- View ----}
+
+{---- Signals ----}
