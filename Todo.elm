40476fb44cfab18c4f3a2027d4e4b1f8fd762fbf
Adriano Ferrari <adriano.ferrari@gmail.com>
2015-01-10 07:59:41 -0500

    Elm code compiles.

--- Todo.elm
+++ Todo.elm
@@ -24,25 +24,26 @@ type alias Todo =
 type Delta  = NoOp
             | TodoAdd Todo
             | TodoDelete Int
-            | TodoToggle Int
             | SetFilter Visibility
+            {-| TodoToggle Int-}
 
 update : State -> Delta -> State
 update state delta =
   case delta of
-    NoOp -> state
-
     TodoAdd todo ->
-      { state | state.todos <- state.todos ++ todo }
+      { state | todos <- state.todos ++ [todo] }
 
     TodoDelete id ->
-      { state | state.todos <- List.filter (\t -> not (t.id == id)) state.todos }
+      { state | todos <- List.filter (\t -> not (t.id == id)) state.todos }
 
-    TodoToggle id ->
-      { state | List.map (\t -> if t.id then t.completed <- not t.complete) state.todos}
+    {-TodoToggle id ->
+      { state | todos <- List.map (\t -> if t.id then (t.completed <- not t.complete)) todos}
+        -}
 
     SetFilter vis ->
       { state | visibility <- vis }
+
+    NoOp -> state
 
 {---- View ----}
 
